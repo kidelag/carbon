25789cf495a11572d1de0c1bd4a85cf2d59f24f0
Vaalgtir <e.eniona2@gmail.com>
2023-06-07 12:38:56 +0200

    feat: adding fixtures

--- carbon-back/src/consultant/entities/consultant.entity.ts
+++ carbon-back/src/consultant/entities/consultant.entity.ts
@@ -1,43 +1,49 @@
-import {Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn} from "typeorm";
-import {User} from "../../users/users.entity";
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { User } from "../../users/entities/users.entity";
 
 @Entity()
 export class Consultant {
-    @PrimaryGeneratedColumn("uuid")
-    public id: string;
+  @PrimaryGeneratedColumn("uuid")
+  public id: string;
 
-    @OneToOne(() => User)
-    @JoinColumn()
-    user: User
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
 
-    @Column({
-        nullable: false,
-        type: "int",
-    })
-    public tjm: number;
+  @Column({
+    nullable: false,
+    type: "int",
+  })
+  public tjm: number;
 
-    @Column({
-        nullable: false,
-        type: "int",
-    })
-    public salary: number;
+  @Column({
+    nullable: false,
+    type: "int",
+  })
+  public salary: number;
 
-    @Column({
-        nullable: false,
-        type: "text"
-    })
-    public address: string;
+  @Column({
+    nullable: false,
+    type: "text",
+  })
+  public address: string;
 
-    @Column({
-        nullable: false,
-        type: "char",
-        length: 10
-    })
-    public tel: string;
+  @Column({
+    nullable: false,
+    type: "char",
+    length: 255,
+  })
+  public tel: string;
 
-    @Column({
-        nullable: false,
-        type: "datetime",
-    })
-    public startDate: Date;
+  @Column({
+    nullable: false,
+    type: "datetime",
+  })
+  public startDate: Date;
 }
