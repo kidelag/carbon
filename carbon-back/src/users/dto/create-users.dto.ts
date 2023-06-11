07408ea832a180d612909336cfe02724be29debd
Vaalgtir <e.eniona2@gmail.com>
2023-06-11 23:00:53 +0200

    fix: fixtures

--- carbon-back/src/users/dto/create-users.dto.ts
+++ carbon-back/src/users/dto/create-users.dto.ts
@@ -1,25 +1,31 @@
-import {Role} from "../../authentication/authentication.enum";
-import {IsDate, IsDateString, IsDefined, IsEmail, IsString} from "class-validator";
-import {Type} from "class-transformer";
+import { Role } from "../../authentication/authentication.enum";
+import {
+  IsDate,
+  IsDateString,
+  IsDefined,
+  IsEmail,
+  IsString,
+} from "class-validator";
+import { Type } from "class-transformer";
 
 export class CreateUsersDto {
-    @IsDefined()
-    @IsEmail()
-    public email: string;
+  @IsDefined()
+  @IsEmail()
+  public email: string;
 
-    @IsDefined()
-    public firstname: string;
+  @IsDefined()
+  public firstname: string;
 
-    @IsDefined()
-    public lastname: string;
+  @IsDefined()
+  public lastname: string;
 
-    @IsDefined()
-    public password: string;
+  @IsDefined()
+  public password: string;
 
-    @IsDate()
-    @Type(() => Date)
-    public birthdate: Date;
+  @IsDate()
+  @Type(() => Date)
+  public birthdate: Date;
 
-    @IsDefined()
-    public role: Role;
+  @IsDefined()
+  public role: Role;
 }
